f4643e15ac6f624b6079d983bb1de3e5782af62f
wzp <wzp@qq.com>
2021-01-30 18:00:23 +0800

    新增测试

---
+++ src/main/java/com/wzp/controller/TestController02.java
@@ -0,0 +1,10 @@
+package com.wzp.controller;
+
+public class TestController02 {
+    public void test(){
+        System.out.println("hello");
+    }
+    public void test02(){
+        System.out.println("111");
+    }
+}
